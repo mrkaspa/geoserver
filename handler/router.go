0bb05a72d4eb34161b1b3fc5ce2c3ca4b5ae2a4a
mrkaspa <michel.ingesoft@gmail.com>
2016-07-25 14:30:25 -0500

    negroni integrated

--- handler/router.go
+++ handler/router.go
@@ -1,14 +1,28 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/mrkaspa/geoserver/ws"
+	"github.com/urfave/negroni"
 )
 
-func NewRouter() *mux.Router {
-	router := mux.NewRouter()
-	router.HandleFunc("/ws/{username}", ws.Adapter)
-	router.HandleFunc("/near", nearHandler).Methods("POST")
-	router.HandleFunc("/store", storeHandler).Methods("POST")
-	return router
+type fakeRouter struct {
+	mux *mux.Router
+}
+
+func NewRouter() http.Handler {
+	n := negroni.Classic()
+	router := &fakeRouter{mux: mux.NewRouter()}
+	router.mux.HandleFunc("/ws/{username}", ws.Adapter)
+	router.mux.HandleFunc("/near", nearHandler).Methods("POST")
+	router.mux.HandleFunc("/store", storeHandler).Methods("POST")
+	n.Use(router)
+	return n
+}
+
+func (router *fakeRouter) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	router.mux.ServeHTTP(w, r)
+	next(w, r)
 }
--- test/suite_test.go
+++ test/suite_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -30,9 +29,7 @@ func Test(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	router := handler.NewRouter()
-	http.Handle("/", router)
-	ts = httptest.NewServer(router)
+	ts = httptest.NewServer(handler.NewRouter())
 })
 
 var _ = AfterSuite(func() {
